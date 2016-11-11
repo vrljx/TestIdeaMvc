eab3fdff729aedcc8505a064401cf3281aff6e91
vrljx <vrljx@126.com>
2016-11-11 17:47:27 +0800

    clone

---
+++ src/main/java/com/ljx/chapter03/test043/Client043.java
@@ -0,0 +1,11 @@
+package com.ljx.chapter03.test043;
+
+/**
+ * @Created by taylor on 2016/11/11.
+ * @DESC    避免对象的浅拷贝
+ * 一个类实现了Clonealbe接口就说明具备了拷贝能力
+ * 拷贝是在内存中进行，性能比较好，比new强
+ * 但是对于 大对象的拷贝，容易出现拷贝不完整的问题。
+ */
+public class Client043 {
+}
